11f9a7d1d14eb265ed3ca4825b853d1e341a0a2a
Benjamin Adzovic Novak <benjamin.adzovic@gmail.com>
2018-06-26 13:21:37 +0100

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,3 @@
 module.exports = {
   mongoURI: 'mongodb://admin:password@localhost:27017/dev?authSource=admin',
-  ssl_key: __dirname + '/local_certificate/key.pem',
-  ssl_cert: __dirname + '/local_certificate/cert.pem',
-  ssl_passphrase: '',
 }
